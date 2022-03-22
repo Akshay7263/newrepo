2e1d3f22cd056362083bd2d0a89c995530b7ca26
Akshay Girme <girmeakshay62@gmail.com>
2022-03-22 14:26:28 +0530

    4th commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//when every thing is load on page
+$(window).on('load',()=>{
+    alert("window is load");
+});
+
+//when dom is ready
+$( document ).ready(function() {
+    alert('ready')
+});
+
+$('.btn').click(()=>{
+    alert("hii");
+})
